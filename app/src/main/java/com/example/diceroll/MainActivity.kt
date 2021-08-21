09dcea2cbcd074112a89367f3cfe810c927c1d5d
Clint-Mathews <mathewsclint28@gmail.com>
2021-08-21 09:43:50 +0530

    Updated dice image and functionality

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -2,12 +2,11 @@ package com.example.diceroll
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 /**
- Dice Class
+Dice Class
  **/
 class Dice(private val numberOfSides: Int) {
     fun roll(): Int {
@@ -17,19 +16,29 @@ class Dice(private val numberOfSides: Int) {
 
 class MainActivity : AppCompatActivity() {
     /**
-     Roll Dice function
+    Roll Dice function
      **/
     private fun rollDice() {
         // Toast message
-        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-        toast.show()
+//        val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+//        toast.show()
         // Create a Dice Obj
         var diceObj = Dice(6);
         var dataToShow = diceObj.roll();
-
+        var diceImage: ImageView = findViewById(R.id.imageView);
         // Show the dice object values
-        var textViewShow: TextView = findViewById(R.id.textView);
-        textViewShow.text = dataToShow.toString();
+        var result = when (dataToShow) {
+            1 -> R.drawable.dice_1;
+            2 -> R.drawable.dice_2;
+            3 -> R.drawable.dice_3;
+            4 -> R.drawable.dice_4;
+            5 -> R.drawable.dice_5;
+            else -> R.drawable.dice_6;
+        }
+//        Set Image based on roll
+        diceImage.setImageResource(result);
+//        Update the image description
+        diceImage.contentDescription = result.toString();
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,5 +48,14 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
+        val rollButton2: Button = findViewById(R.id.button2)
+        rollButton2.setOnClickListener {
+            rollDice()
+            rollDice()
+        }
+//        Initial Load
+//        rollDice()
+        var diceImage: ImageView = findViewById(R.id.imageView);
+        diceImage.setImageResource(R.drawable.dice_1);
     }
 }
